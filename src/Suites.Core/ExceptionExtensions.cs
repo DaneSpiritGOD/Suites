d283c41fc24ba27b15271a9fc9433ae0a65a8332
Dane <838652739@qq.com>
2018-10-16 16:39:44 +0800

    小修复了Assert帮助类

--- src/Suites.Core/ExceptionExtensions.cs
+++ src/Suites.Core/ExceptionExtensions.cs
@@ -16,7 +16,11 @@ namespace System
 
         public static string Assert(string arg, string exceptionShowName, [CallerMemberName]string methodName = null)
         {
-            if (string.IsNullOrEmpty(arg)) throw new StringNullOrEmptyException($"variable: {exceptionShowName} in method: {methodName}");
+            if (string.IsNullOrEmpty(arg))
+            {
+                throw new StringNullOrEmptyException($"variable: {exceptionShowName} in method: {methodName}");
+            }
+
             return arg;
         }
     }
@@ -27,7 +31,11 @@ namespace System
 
         public static string Assert(string arg, string exceptionShowName, [CallerMemberName]string methodName = null)
         {
-            if (string.IsNullOrWhiteSpace(arg)) throw new StringNullOrWhiteSpaceException($"variable: {exceptionShowName} in method: {methodName}");
+            if (string.IsNullOrWhiteSpace(arg))
+            {
+                throw new StringNullOrWhiteSpaceException($"variable: {exceptionShowName} in method: {methodName}");
+            }
+
             return arg;
         }
     }
@@ -49,32 +57,46 @@ namespace System
 
         public static T Assert(T arg, T low, T high, string exceptionShowName, [CallerMemberName]string methodName = null)
         {
-            if (arg.CompareTo(high) > 0 || arg.CompareTo(low) < 0) throw new NumberOutOfRangeException<T>(
+            if (arg.CompareTo(high) > 0 || arg.CompareTo(low) < 0)
+            {
+                throw new NumberOutOfRangeException<T>(
                 arg,
                 low,
                 high,
                 $"variable: {exceptionShowName} in method: {methodName}");
+            }
+
             return arg;
         }
     }
 
     public class NotTrueException : RuntimeException
     {
-        public NotTrueException(string predictName) : base($"Prediction: {predictName} is not true.") { }
+        public NotTrueException(string msg) : base($"{msg} is not true.") { }
 
         public static void Assert(Func<bool> predict, string predictName, [CallerMemberName]string methodName = null)
         {
-            if (!predict()) throw new NotTrueException($"predict: {predictName} in method: {methodName}");
+            if (!predict())
+            {
+                throw new NotTrueException($"predict: {predictName} in method: {methodName}");
+            }
         }
 
         public static void Assert(bool predictResult, string predictName, [CallerMemberName]string methodName = null)
         {
-            if (!predictResult) throw new NotTrueException($"predict: {predictName} in method: {methodName}");
+            if (!predictResult)
+            {
+                throw new NotTrueException($"predict: {predictName} in method: {methodName}");
+            }
         }
 
         public static T Assert<T>(T value, bool condition, string valueName, [CallerMemberName]string methodName = null)
         {
-            if (!condition) throw new NotTrueException($"{valueName} does not meet the condition in method: {methodName}.");
+            if (!condition)
+            {
+                throw new NotTrueException($"the condition of {valueName} in method: {methodName}");
+            }
+
             return value;
         }
     }
