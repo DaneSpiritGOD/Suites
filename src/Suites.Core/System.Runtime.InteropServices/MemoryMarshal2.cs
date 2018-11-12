1612742f16fff846ab4e823884db6fe0c4f034a3
Dane <838652739@qq.com>
2018-11-12 19:55:44 +0800

    完善MemoryMarshal2逻辑

--- src/Suites.Core/System.Runtime.InteropServices/MemoryMarshal2.cs
+++ src/Suites.Core/System.Runtime.InteropServices/MemoryMarshal2.cs
@@ -7,14 +7,14 @@ namespace System.Runtime.InteropServices
     public static class MemoryMarshal2
     {
         /// <summary>
-        /// 检查memory的内存对象是否是数组切片，如果是，则返回数组；否则利用ToArray()返回内存拷贝。
+        /// 检查memory的内存对象是否是完整数组（索引与长度均一致），如果是，则直接返回数组；否则利用ToArray()返回内存拷贝。
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="memory"></param>
         /// <returns></returns>
         public static T[] GetZeroIndexedArrayOrCopyedContent<T>(this Memory<T> memory)
         {
-            return MemoryMarshal.TryGetArray(memory, out ArraySegment<T> segment) ? segment.Array : memory.ToArray();
+            return (MemoryMarshal.TryGetArray(memory, out ArraySegment<T> segment) && segment.Offset == 0 && segment.Array.Length == memory.Length) ? segment.Array : memory.ToArray();
         }
     }
 }
