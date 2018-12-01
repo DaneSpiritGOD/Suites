3542feb3936c5567f6a8dba61141b9f22c9c39af
Dane@shdai <shdai@abcft.com>
2018-12-01 18:08:25 +0800

    修改AvalonEdit的Assem信息

---
+++ src/Suites.Wpf.AvalonEdit/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://suites.com/AvalonEdit", "Suites.Wpf.AvalonEdit.Behaviors")]
+[assembly: XmlnsPrefix("http://suites.com/AvalonEdit", "swaa")]
