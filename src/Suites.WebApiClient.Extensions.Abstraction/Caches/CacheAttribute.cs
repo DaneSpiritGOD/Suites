11c4cc0e49f71d82408c8d351068206d2c46acf9
Dane@shdai <shdai@abcft.com>
2019-03-01 15:41:32 +0800

    🎨 重构...

---
+++ src/Suites.WebApiClient.Extensions.Abstraction/Caches/CacheAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiClient.Attributes;
+using WebApiClient.Contexts;
+
+namespace WebApiClient.Extensions.Caches
+{
+    public class CacheAttribute : ApiActionAttribute
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="secAge">缓存有效时间，单位：秒</param>
+        public CacheAttribute(double secAge)
+        {
+            NotTrueException.Assert(secAge > 0, nameof(secAge));
+            SecAge = TimeSpan.FromSeconds(secAge);
+        }
+
+        public TimeSpan SecAge { get; }
+
+        public override Task BeforeRequestAsync(ApiActionContext context)
+            => TaskEx.CompletedTask;
+    }
+}
---
+++ src/Suites.WebApiClient.Extensions/Caches/DefaultHttpApiFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiClient
+{
+    public sealed class DefaultHttpApiFactory<TInterface>
+        : IHttpApiFactory<TInterface>
+        where TInterface : class, IHttpApi
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DefaultHttpApiFactory(IServiceProvider provider)
+        {
+            _serviceProvider = NamedNullException.Assert(provider, nameof(provider));
+        }
+
+        public void ConfigureHttpApiConfig(Action<HttpApiConfig> options) 
+            => throw new NotImplementedException();
+
+        public TInterface CreateHttpApi()
+            => _serviceProvider.GetRequiredService<TInterface>();
+    }
+}
