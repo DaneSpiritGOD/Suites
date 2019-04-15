9b2307ef834b5dd6e1b713850bbc479002091ac5
Dane@shdai <shdai@abcft.com>
2019-04-15 13:32:31 +0800

    新增注释

--- src/Suites.Utils/System/TypeExtensions.cs
+++ src/Suites.Utils/System/TypeExtensions.cs
@@ -2,6 +2,12 @@
 {
     public static class TypeExtensions
     {
+        /// <summary>
+        /// 判断一个泛型类C<T>是否继承自C<>
+        /// </summary>
+        /// <param name="toCheck"></param>
+        /// <param name="generic"></param>
+        /// <returns></returns>
         public static bool IsSubclassOfRawGeneric(this Type toCheck, Type generic)
         {
             while (toCheck != null && toCheck != typeof(object))
