03da6e4001e53e457beedb87e702f320e7af28fb
Dane@shdai <shdai@abcft.com>
2019-09-25 17:49:38 +0800

    添加 akavache 扩展项目

---
+++ src/Suites.Akavache/DoNotExistSuchNameMediaException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Suites.Akavache.Properties.Resources;
+
+namespace Suites.Akavache
+{
+    public class DoNotExistSuchNameMediaException : Exception
+    {
+        public DoNotExistSuchNameMediaException(string name) 
+            : base(string.Format(DoNotExistSuchNameMediaExceptionString,name))
+        {
+        }
+    }
+}
---
+++ src/Suites.Akavache/IAkavacheManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akavache;
+
+namespace Suites.Akavache
+{
+    public interface IAkavacheManager : IDisposable
+    {
+        void AddPersistenceMedia(AkaCacheOptions options);
+        IBlobCache GetMedia(string name);
+    }
+}
