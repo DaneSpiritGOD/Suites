0bdd9cce4c7393bfe40a208c69e1efe90cf71144
Dane@shdai <shdai@abcft.com>
2019-09-26 14:22:29 +0800

    改用`Microsoft.Xaml.Behaviors.Wpf`

--- src/Suites.Wpf.AvalonEdit/Behaviors/AvalonEditBehavior.cs
+++ src/Suites.Wpf.AvalonEdit/Behaviors/AvalonEditBehavior.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Interactivity;
 using ICSharpCode.AvalonEdit;
+using Microsoft.Xaml.Behaviors;
 
 namespace Suites.Wpf.AvalonEdit.Behaviors
 {
